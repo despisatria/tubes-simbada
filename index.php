9f51a491edd0ac482a4341fd1760c54489037e3d
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2017-12-09 14:40:35 +0700

    design home

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hello, world!</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="topbar">
+        <div class="container">
+          <div class="row top-bar-list top-bar-line">
+            <ul class="col-4">
+              <li><img src="" alt="fb"></li>
+              <li><img src="" alt="tw"></li>
+              <li><img src="" alt="insta"></li>
+            </ul>
+            <ul class="topbar-nav col-8 text-right">
+              <li class="topbar-item active">
+                <a class="topbar-link" href="#">Login <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="topbar-item">
+                <a class="topbar-link" href="#">Daftar</a>
+              </li>
+              <li class="topbar-item">
+                <a class="topbar-link" href="#">Konfirmasi Pembayaran</a>
+              </li>
+              <li class="topbar-item">
+                <a class="topbar-link" href="#">Keranjang</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container header-web top-bar-line">
+          <div class="col-4">
+            <form class="form-inline my-2 my-md-0">
+              <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </form>
+          </div>
+          <div class="col-4 text-center">
+            <a class="navbar-brand" href="#">HIJAB</a>
+          </div>
+          <div class="col-4 text-right">
+            <a href="#">Keranjang</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </div>
+      </nav>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbar">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">BERANDA <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">BELANJA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CARA BELANJA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">BLOG</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="container">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="assets/img/slider1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="assets/img/slider2.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="assets/img/slider3.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+
+    <script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
